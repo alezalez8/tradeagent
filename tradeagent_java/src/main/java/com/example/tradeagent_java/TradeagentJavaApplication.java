f136502da02d9331971e9097e0c441c5d5678989
alezalez8 <5446943@ukr.net>
2022-08-14 13:16:41 +0300

    java project initial

---
+++ tradeagent_java/src/main/java/com/example/tradeagent_java/TradeagentJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tradeagent_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeagentJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeagentJavaApplication.class, args);
+    }
+
+}
